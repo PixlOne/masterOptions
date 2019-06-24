0f7274148035082588df9fd787e905845ca2f5f7
PixlOne <8843371+PixlOne@users.noreply.github.com>
2019-06-24 13:47:27 -0400

    Fixed some bugs with connection events

--- src/masterd/DeviceFinder.cpp
+++ src/masterd/DeviceFinder.cpp
@@ -26,23 +26,23 @@ void find_device()
 void DeviceFinder::addDevice(const char *path)
 {
     SCANNING_DEVICE = true;
-    const int max_tries = 5;
-    const int try_delay = 2000000;
-    for(int i = 0; i < max_tries; i++)
+    const int max_tries = 10;
+    const int try_delay = 500000;
+    try
     {
-        try
+        HIDPP::SimpleDispatcher dispatcher(path);
+        bool has_receiver_index = false;
+        for (HIDPP::DeviceIndex index: {
+            HIDPP::DefaultDevice, HIDPP::CordedDevice,
+            HIDPP::WirelessDevice1, HIDPP::WirelessDevice2,
+            HIDPP::WirelessDevice3, HIDPP::WirelessDevice4,
+            HIDPP::WirelessDevice5, HIDPP::WirelessDevice6})
         {
-            HIDPP::SimpleDispatcher dispatcher(path);
-            bool has_receiver_index = false;
-            for (HIDPP::DeviceIndex index: {
-                    HIDPP::DefaultDevice, HIDPP::CordedDevice,
-                    HIDPP::WirelessDevice1, HIDPP::WirelessDevice2,
-                    HIDPP::WirelessDevice3, HIDPP::WirelessDevice4,
-                    HIDPP::WirelessDevice5, HIDPP::WirelessDevice6})
+            //Skip wireless devices if default device (receiver) has failed
+            if (!has_receiver_index && index == HIDPP::WirelessDevice1)
+                break;
+            for(int i = 0; i < max_tries; i++)
             {
-                //Skip wireless devices if default device (receiver) has failed
-                if (!has_receiver_index && index == HIDPP::WirelessDevice1)
-                    break;
                 try
                 {
                     HIDPP::Device d(&dispatcher, index);
@@ -51,47 +51,48 @@ void DeviceFinder::addDevice(const char *path)
                         has_receiver_index = true;
                     if (!strcmp(d.name().c_str(), DEVICE_NAME))
                     {
-                        DeviceHandler* dh = new DeviceHandler(std::string(path), index);
-                        handlers.push_back(new handler_pair {
-                            dh, std::async(std::launch::async, &DeviceHandler::start, dh)
+                        DeviceHandler *dh = new DeviceHandler(std::string(path), index);
+                        handlers.push_back(new handler_pair{
+                                dh, std::async(std::launch::async, &DeviceHandler::start, dh)
                         });
+                        printf("%s detected: device %d on %s\n", DEVICE_NAME, index, path);
                     }
                     i = max_tries;
                 }
                 catch (HIDPP10::Error &e)
                 {
-                    if(i >= max_tries)
+                    if (e.errorCode() != HIDPP10::Error::UnknownDevice && e.errorCode() != HIDPP10::Error::InvalidSubID)
                     {
-                        if (e.errorCode() != HIDPP10::Error::UnknownDevice &&
-                            e.errorCode() != HIDPP10::Error::InvalidSubID)
+                        if (i >= max_tries-1)
                             fprintf(stderr, "Error while querying %s, wireless device %d: %s\n",
                                     path, index, e.what());
-                    }
-                    else usleep(try_delay);
+                        else usleep(try_delay);
+                    } else break;
                 }
                 catch (HIDPP20::Error &e)
                 {
-                    if(i >= max_tries)
+                    if (e.errorCode() != HIDPP20::Error::UnknownDevice && i >= max_tries)
                     {
-                        if (e.errorCode() != HIDPP20::Error::UnknownDevice && i >= max_tries)
+                        if (i >= max_tries-1)
                             fprintf(stderr, "Error while querying %s, device %d: %s\n",
                                     path, index, e.what());
-                    }
-                    else usleep(try_delay);
+                        else usleep(try_delay);
+                    } else break;
+
                 }
                 catch (HIDPP::Dispatcher::TimeoutError &e)
                 {
-                    if(i >= max_tries)
+                    if (i >= max_tries)
                         fprintf(stderr, "Device %s (index %d) timed out\n", path, index);
                     else usleep(try_delay);
                 }
             }
         }
-        catch(HIDPP::Dispatcher::NoHIDPPReportException &e) {}
-        catch(std::system_error &e)
-        {
-            /*fprintf(stderr, "Failed to open %s: %s\n", path, e.what());*/
-        }
+    }
+    catch(HIDPP::Dispatcher::NoHIDPPReportException &e) {}
+    catch(std::system_error &e)
+    {
+        /*fprintf(stderr, "Failed to open %s: %s\n", path, e.what());*/
     }
     SCANNING_DEVICE = false;
 }
@@ -103,6 +104,7 @@ void DeviceFinder::removeDevice(const char* path)
     {
         if(!strcmp((*it)->handler->path.c_str(), path))
         {
+            printf("%s on %s removed\n", DEVICE_NAME, (*it)->handler->path.c_str());
             (*it)->handler->DeviceRemoved = true;
             (*it)->future.wait();
             handlers.erase(it);
--- src/masterd/Diverter.cpp
+++ src/masterd/Diverter.cpp
@@ -11,30 +11,40 @@ using namespace HIDPP20;
 void divert_buttons(const char* path, HIDPP::DeviceIndex index)
 {
     std::unique_ptr<HIDPP::Dispatcher> dispatcher;
-    try
+    const int max_tries = 5;
+    const int try_delay = 200000;
+    for(int i = 0; i < max_tries; i++)
     {
-        dispatcher = std::make_unique<HIDPP::SimpleDispatcher>(path);
-    }
-    catch (std::exception &e)
-    {
-        fprintf(stderr, "Failed to open device: %s.\n", e.what());
-        std::exit(EXIT_FAILURE);
-    }
-    Device dev(dispatcher.get(), index);
-    try
-    {
-        IReprogControlsV4 irc4(&dev);
-        for(uint16_t cid : DIVERT_OPTIONS)
+        try
         {
-            uint8_t flags = 0;
-            flags |= IReprogControlsV4::ChangeTemporaryDivert;
-            flags |= IReprogControlsV4::TemporaryDiverted;
-            irc4.setControlReporting(cid, flags, cid);
+            dispatcher = std::make_unique<HIDPP::SimpleDispatcher>(path);
+            break;
+        }
+        catch (std::exception &e)
+        {
+            if(i >= max_tries - 1)
+                fprintf(stderr, "Failed to open device: %s.\n", e.what());
         }
     }
-    catch(Error &e)
+    Device dev(dispatcher.get(), index);
+    for(int i = 0; i < max_tries; i++)
     {
-        fprintf(stderr, "Could not divert buttons. Error code: %d: %s\n", e.errorCode(), e.what());
-        std::exit(e.errorCode());
+        try
+        {
+            IReprogControlsV4 irc4(&dev);
+            for (uint16_t cid : DIVERT_OPTIONS)
+            {
+                uint8_t flags = 0;
+                flags |= IReprogControlsV4::ChangeTemporaryDivert;
+                flags |= IReprogControlsV4::TemporaryDiverted;
+                irc4.setControlReporting(cid, flags, cid);
+            }
+            break;
+        }
+        catch (Error &e)
+        {
+            if(i >= max_tries - 1)
+                fprintf(stderr, "Could not divert buttons. Error code: %d: %s\n", e.errorCode(), e.what());
+        }
     }
 }
